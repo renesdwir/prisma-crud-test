69f033c04e280ed391773e194bccbc59618fc2b4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-12 23:14:06 +0700

    get products

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
-const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.send(products);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
