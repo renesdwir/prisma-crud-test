8fcfa6897210ddeb4bde79e197400bdd7124ac0c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-14 20:36:22 +0700

    default /

--- src/index.js
+++ src/index.js
@@ -12,9 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("welcome to crud api with prisma");
 });
-
 app.get("/products", async (req, res) => {
   try {
     const products = await prisma.product.findMany();
@@ -32,7 +31,6 @@ app.get("/products/:id", async (req, res) => {
     console.log(error);
   }
 });
-
 app.post("/products", async (req, res) => {
   try {
     const { name, price, description, image } = req.body;
@@ -49,7 +47,7 @@ app.delete("/products/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const product = await prisma.product.delete({ where: { id } });
-    res.send({ message: "delete data successfullt", data: product });
+    res.send({ message: "delete data successfully", data: product });
   } catch (error) {
     console.log(error);
   }
